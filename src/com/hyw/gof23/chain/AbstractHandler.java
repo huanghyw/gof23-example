ceeaeabcabeb09ac2b2e6d9953878594c5209f04
huangyiwei <huangyiwei@finupgroup.com>
2019-02-27 11:14:18 +0800

    add chain

--- src/com/hyw/gof23/chain/AbstractHandler.java
+++ src/com/hyw/gof23/chain/AbstractHandler.java
@@ -1,5 +1,8 @@
 package com.hyw.gof23.chain;
 
+/**
+ * 定义节点的公共逻辑
+ */
 public abstract class AbstractHandler implements Handler {
     private Handler next = null;
 
@@ -7,16 +10,22 @@ public abstract class AbstractHandler implements Handler {
         this.next = next;
     }
 
+
+    // 节点处理
     @Override
     public void handler() {
+        // 先处理本节点的工作
         this.work();
         if (this.next == null){
+            // 如果当前节点处理完成后，没有定义下级节点，则本节点为流程最终节点
             System.out.println("当前节点为末节点，流程结束！");
             return;
         }else{
+            // 触发下一节点的处理
             this.next.handler();
         }
     }
 
+    // 每个节点具体的工作
     protected abstract void work();
 }
--- src/com/hyw/gof23/chain/Main.java
+++ src/com/hyw/gof23/chain/Main.java
@@ -1,10 +1,20 @@
 package com.hyw.gof23.chain;
 
 import com.hyw.gof23.chain.impl.Begin;
+import com.hyw.gof23.chain.impl.FlowC1;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("------From Begin Node---------");
+
+        // 创建一个Begin节点，并开始处理
         Handler begin = new Begin();
         begin.handler();
+
+        System.out.println("------From FlowC1 Node---------");
+
+        // 创建一个FlowC1节点，并开始处理
+        Handler flowC1 = new FlowC1();
+        flowC1.handler();
     }
 }
